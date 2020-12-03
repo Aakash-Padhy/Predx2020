c147325ca0972d682bbcc9e77e6ddda47999b1c6
sanat12 <56288290+sanat12@users.noreply.github.com>
2020-12-03 23:08:22 +0530

    Add files via upload

---
+++ PredxHack2020/src/App.js
@@ -0,0 +1,51 @@
+import React,{useEffect} from 'react';
+import {Router,Route,Redirect,Switch} from 'react-router-dom';
+import {history} from './helpers/history';
+import {useSelector,useDispatch} from 'react-redux';
+import './App.css';
+import {clearErrors} from './redux/actions/errorActions';
+import {loadUser} from './redux/actions/authAction';
+import Auth from './components/Auth/Auth';
+import Home from './components/Home/Home';
+import { useState } from 'react';
+
+
+
+function App() {
+
+  const dispatch = useDispatch()
+   history.listen((location, action) => {
+    // clear alert on location change
+    dispatch(clearErrors())
+  });
+  useEffect(()=>{
+    dispatch(loadUser())
+  },[])
+
+  console.log("rerendering")
+ 
+  return (
+    <div className='app'>
+        <div className='main-body'>
+        <Router history={history}>
+          <Switch>
+                <Route exact path='/'>
+                  <Home/>
+                </Route>
+                <Route path="/Auth" >
+                    <Auth/>
+                </Route>
+                <Route path="/register">
+                    <Auth/>
+                </Route>
+                <Route path="/login">
+                    <Auth/>
+                </Route>
+            </Switch>
+          </Router>
+        </div>
+    </div>
+  );
+}
+
+export default App;
